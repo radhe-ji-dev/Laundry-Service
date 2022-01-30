13de7c8ce06f1566023d80ebb77ac82172637efc
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-30 10:59:39 +0530

    app chnagesd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
-import React,{useState, createContext} from "react";
+import React, { useState, createContext } from "react";
 import Login from "./login/login";
 import Register from "./register/register";
 import Createorderhome from "./createorder/createorderhome";
 import Ordercreation from "./createorder/ordercreation";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Getorder from "./getorder/Getorder";
 
 export const store = createContext();
 
-
 function App() {
-  const[token, setToken] = useState(null);
+  const [token, setToken] = useState(null);
   return (
-      <div className="App">
-        <store.Provider value={[token,setToken]}>
-          <BrowserRouter>
-            <Routes>
-              <Route exact path="/" element={<Login/>} />
-              <Route exact path="/register" element={<Register/>} />
-              <Route exact path="/createorder" element={<Createorderhome/>} />
-              <Route exact path="/ordercreate" element={<Ordercreation/>} />
-            </Routes>
-          </BrowserRouter>
-        </store.Provider>
-      </div>
+    <div className="App">
+      <store.Provider value={[token, setToken]}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/createorder" element={<Createorderhome />} />
+            <Route exact path="/ordercreate" element={<Ordercreation />} />
+            <Route exact path="/getorder" element={<Getorder />} />
+          </Routes>
+        </BrowserRouter>
+      </store.Provider>
+      {/*<Getorder/>*/}
+    </div>
   );
 }
 
